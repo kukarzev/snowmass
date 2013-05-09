70c9ddcf4a63f62cfe50adbf4204c0f7e6c6c8ea
Gena Kukartsev <kukarzev@gmail.com>
2013-05-09 18:58:19 -0500

    build system added

---
+++ DoubleHiggs/src/SnowmassApp.cpp
@@ -0,0 +1,27 @@
+/*
+  
+  Analysis application to loop over Delphes output event files
+  Currently set up with Snowmass 2013 fastsim studies in mind
+
+  Authors:
+           Gena Kukartsev
+           John Stupak
+
+  2013
+
+ */
+
+
+
+#include<iostream>
+
+
+
+int main(int argc, char * argv[]){
+
+  std::cout << std::endl;
+  std::cout << "Welcome to Snowmass!" << std::endl;
+  std::cout << std::endl;
+
+  return 0;
+}
